37b42183ccfa43a8ac678b2d9e42d19c6d00690e
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-09-09 13:14:58 -0400

    Renaming

--- helmfile/contextExample.sh
+++ helmfile/contextExample.sh
@@ -7,4 +7,4 @@ export KEYVAULT_NAME=dtssecrets
 export K8S_CLUSTER_NAME=EsDCDTSDevRG-K8S
 export K8S_RG_NAME=EsDCDTSDevRG
 export BASE_DOMAIN=dev.dts-stn.com
-export APP_NAME=single-tier-application
+export APP_NAME=sc-digital-centre
