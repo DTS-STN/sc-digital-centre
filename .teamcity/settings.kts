a67e1990c766c40a7f3dbf8416d797a0a6a9ec9e
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-09-10 11:12:54 -0400

    Adding settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -25,4 +25,68 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2020.2"
 
 project {
+
+    vcsRoot(HttpsGithubComDtsStnScDigitalCentre)
+    buildType(Build)
 }
+
+object HttpsGithubComDtsStnScDigitalCentre : GitVcsRoot({
+    name = "https://github.com/DTS-STN/sc-digital-centre"
+    url = "git@github.com:DTS-STN/sc-digital-centre.git"
+    branch = "refs/heads/main"
+    authMethod = uploadedKey {
+        userName = "git"
+        uploadedKey = "dtsrobot"
+    }
+})
+
+object Build: BuildType({
+   name = "Build"
+   description = "Continuous integration"
+
+   vcs {
+     root(HttpsGithubComDtsStnScDigitalCentre)
+   }
+   
+   steps {
+        dockerCommand {
+            name = "Build & Tag Docker Image"
+            commandType = build {
+                source = file {
+                    path = "Dockerfile"
+                }
+                namesAndTags = "%env.ACR_DOMAIN%/%env.PROJECT%:%env.DOCKER_TAG%"
+                commandArgs = "--pull --build-arg BUILD_DATE=%system.build.start.date% --build-arg TC_BUILD=%build.number% --build-arg CONTENT_API=https://www.canada.ca/api/assets/decd-endc/content-fragments/ --build-arg NEXT_PUBLIC_FEEDBACK_API=https://alphasite.dts-stn.com/api/feedback"
+            }
+        }
+        script {
+            name = "Login to Azure and ACR"
+            scriptContent = """
+                az login --service-principal -u %TEAMCITY_USER% -p %TEAMCITY_PASS% --tenant %env.TENANT-ID%
+                az account set -s %env.SUBSCRIPTION%
+                az acr login -n MTSContainers
+            """.trimIndent()
+        }
+        dockerCommand {
+            name = "Push Image to ACR"
+            commandType = push {
+                namesAndTags = "%env.ACR_DOMAIN%/%env.PROJECT%:%env.DOCKER_TAG%"
+            }
+        }
+        script {
+            name = "Deploy w/ Helmfile"
+            scriptContent = """
+                cd ./helmfile
+                az account set -s %env.SUBSCRIPTION%
+                az aks get-credentials --admin --resource-group %env.RG_DEV% --name %env.AKS_DEV%
+                helmfile -e ${'$'}TARGET apply
+            """.trimIndent()
+        }
+    }
+ 
+   triggers {
+      vcs {
+           branchFilter = "+:<default>"
+      }
+   }
+})
