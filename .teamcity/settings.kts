ed9e37cfa67fcdbc68cc9ebc923fe50cbb6008ab
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-09-13 15:28:50 -0400

    test again

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,22 +1,54 @@
-package _Self.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-object Build : BuildType({
-    name = "Build"
-    description = "Continuous integration"
-    paused = true
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
 
-    allowExternalStatus = true
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
 
-    vcs {
-        root(HttpsGithubComDtsStnScDigitalCentre)
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2020.2"
+
+project {
+
+    vcsRoot(HttpsGithubComDtsStnScDigitalCentre)
+    buildType(Build)
+}
+
+object HttpsGithubComDtsStnScDigitalCentre : GitVcsRoot({
+    name = "https://github.com/DTS-STN/sc-digital-centre"
+    url = "git@github.com:DTS-STN/sc-digital-centre.git"
+    branch = "refs/heads/adding-teamcity-steps"
+    authMethod = uploadedKey {
+        userName = "git"
+        uploadedKey = "dtsrobot"
     }
+})
 
-    steps {
+object Build: BuildType({
+   name = "Build"
+   description = "Continuous integration"
+
+   vcs {
+     root(HttpsGithubComDtsStnScDigitalCentre)
+   }
+   
+   steps {
         dockerCommand {
             name = "Build & Tag Docker Image"
             commandType = build {
@@ -51,10 +83,10 @@ object Build : BuildType({
             """.trimIndent()
         }
     }
-
-    triggers {
-        vcs {
-            branchFilter = "+:<default>"
-        }
-    }
+ 
+   triggers {
+      vcs {
+           branchFilter = "+:<default>"
+      }
+   }
 })
