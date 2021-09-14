e7a3d44763ea2636918a548228d80595281faffa
tom.dacey <tom.dacey@TeamCity>
2021-09-14 17:58:19 +0000

    TeamCity change in 'Dev / Sc Digital Centre' project: project parameters were changed

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -17,5 +17,8 @@ changeProject(DslContext.projectId) {
         add {
             param("env.KEYVAULT_READ_PASSWORD", "${'$'}(az keyvault secret show --vault-name dtssecrets --name dts-dev-keyvault-read-password --query value -otsv)")
         }
+        add {
+            param("env.SUBSCRIPTION_ID", """${'$'}(az account show --subscription "MTS" --query 'id' -o tsv)""")
+        }
     }
 }
