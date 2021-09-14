0f4ecdc60b14d771d3eb7e0cfc6a5381d45cad2e
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-09-14 12:15:40 -0400

    trying different context

--- helmfile/context-dev.sh
+++ helmfile/context-dev.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo "exporting context-dev variables"
-export KEYVAULT_READ_USER=$(az keyvault secret show --vault-name dtssecrets --name dts-dev-keyvault-read-user --query value -otsv)
-export KEYVAULT_READ_PASSWORD=$(az keyvault secret show --vault-name dtssecrets --name dts-dev-keyvault-read-password --query value -otsv)
+az account set -s MTS
+export KEYVAULT_NAME=esdcdtsk8sdev-KV
+export KEYVAULT_READ_USER=$(az keyvault secret show --vault-name $KEYVAULT_NAME --name k8s-keyvault-sp-id --query value -otsv)
+export KEYVAULT_READ_PASSWORD=$(az keyvault secret show --vault-name $KEYVAULT_NAME --name k8s-keyvault-sp-pass --query value -otsv)
 export SUBSCRIPTION_ID=$(az account show --subscription "MTS" --query 'id' -o tsv)
 export TENANT_ID=$(az account show --subscription "MTS" --query 'homeTenantId' -o tsv)
-export KEYVAULT_NAME=dtssecrets
-export K8S_CLUSTER_NAME=EsDCDTSDevRG-K8S
-export K8S_RG_NAME=EsDCDTSDevRG
+export K8S_CLUSTER_NAME=ESdCDTSK8SDev-K8S
+export K8S_RG_NAME=ESdCDTSK8SDev
 export BASE_DOMAIN=dev.dts-stn.com
+az aks get-credentials --name $K8S_CLUSTER_NAME --resource-group $K8S_RG_NAME
 export APP_NAME=sc-digital-centre
