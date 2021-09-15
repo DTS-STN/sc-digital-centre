8e9a9d0085bf24a49763d9e2ed315f8afa86fe91
Thomas Dacey <DaceyTom2@gmail.com>
2021-09-15 13:52:36 -0400

    reverting params

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -50,8 +50,6 @@ object Build: BuildType({
         param("env.PROJECT", "sc-digital-centre")
         param("env.BASE_DOMAIN", "bdm-dev.dts-stn.com")
         param("env.BASE_DOMAIN_DEV", "bdm-dev.dts-stn.com")
-        param("env.SUBSCRIPTION", "%vault:dts-sre/azure!/decd-dev-subscription-id%")
-        param("env.AKS_DEV", "ESdCDTSK8SDev-K8S")
     }
     vcs {
         root(HttpsGithubComDtsStnScDigitalCentre)
