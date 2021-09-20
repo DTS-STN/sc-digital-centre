83e8d0a5fdddcb79bba6d292c042ab22b8fbd5f0
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-09-20 11:32:07 -0400

    added colours from sc labs dc design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          secondary: '#7f8c8d', //paragraphs and prefooter bg
+          light: '#cfcfcf',
+        },
+        white: {
+          pure: '#FFFFFF',
+          light: '#fbfbfb',
+        },
+        black: {
+          pure: '#000000',
+          dark: '#262626',
+        },
+        blue: {
+          //primary
+          'primary-deep': '#173451',
+          'bright': '##3498db',
+
+          //secondary
+          'secondary-deep': '#3a6b90',
+          'dull': '#5996b2',
+        },
+        green: {
+          forest: '#28ae60',
+          bright: '#87d37c',
+        },
+        red: {
+          alert: '##d94141',
+        },
+        orange: {
+          alert: '#EE7100',
+        },
+        yellow: '#f39c12',
+        burgundy: '#a24446',
+      },
+    },
   },
   variants: {
     extend: {},
