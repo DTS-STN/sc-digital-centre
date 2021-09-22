6a5c296595d0612f0ccc4fcc5b6898a0834822de
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-09-22 14:42:27 -0400

    adding docker arg.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          footer: '#26374a',
+        },
+      },
+      backgroundImage: () => ({
+        'footer-parliament-image': 'url(../public/landscape.png)',
+      }),
+    },
   },
   variants: {
     extend: {},
