c6f4757207657765939e74e6f36f247825726c43
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-09-23 14:30:18 -0400

    Placing footer at correct location.

--- components/organisms/Layout.js
+++ components/organisms/Layout.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
 import Footer from '../molecules/Footer'
-import DateModified from '../atoms/DateModified'
 import Head from 'next/head'
 
 /**
@@ -15,76 +14,69 @@ export default function Layout({ children }) {
         <div>{children}</div>
       </main>
 
-      <footer>
-        <h2 className="sr-only">siteFooter</h2>
-        <div className="layout-container mt-5">Report a problem</div>
-        <div className="layout-container mb-2">
-          <DateModified date={process.env.NEXT_PUBLIC_BUILD_DATE} />
-        </div>
-        <Footer
-          footerLogoAltText="symbol2"
-          footerLogoImage="/wmms-blk.svg"
-          footerNav1="aboutGovernment"
-          footerNav2="aboutThisSite"
-          links={[
-            {
-              link: 'footerSocialMediaURL',
-              linkText: 'footerSocialMedia',
-            },
-            {
-              link: 'footerMobileAppURL',
-              linkText: 'footerMobileApp',
-            },
-            {
-              link: 'footerTermsAndConditionURL',
-              linkText: 'footerTermsAndCondition',
-            },
-            {
-              link: 'footerPrivacyURL',
-              linkText: 'footerPrivacy',
-            },
-          ]}
-          footerBoxLinks={[
-            {
-              footerBoxlink: 'footerContactUsURL',
-              footerBoxLinkText: 'footerContactUs',
-            },
-            {
-              footerBoxlink: 'footerNewsURL',
-              footerBoxLinkText: 'footerNews',
-            },
-            {
-              footerBoxlink: 'footerPmURL',
-              footerBoxLinkText: 'footerPm',
-            },
-            {
-              footerBoxlink: 'footerDepartmentAgenciesURL',
-              footerBoxLinkText: 'footerDepartmentAgencies',
-            },
+      <Footer
+        footerLogoAltText="symbol2"
+        footerLogoImage="/wmms-blk.svg"
+        footerNav1="aboutGovernment"
+        footerNav2="aboutThisSite"
+        links={[
+          {
+            link: 'footerSocialMediaURL',
+            linkText: 'footerSocialMedia',
+          },
+          {
+            link: 'footerMobileAppURL',
+            linkText: 'footerMobileApp',
+          },
+          {
+            link: 'footerTermsAndConditionURL',
+            linkText: 'footerTermsAndCondition',
+          },
+          {
+            link: 'footerPrivacyURL',
+            linkText: 'footerPrivacy',
+          },
+        ]}
+        footerBoxLinks={[
+          {
+            footerBoxlink: 'footerContactUsURL',
+            footerBoxLinkText: 'footerContactUs',
+          },
+          {
+            footerBoxlink: 'footerNewsURL',
+            footerBoxLinkText: 'footerNews',
+          },
+          {
+            footerBoxlink: 'footerPmURL',
+            footerBoxLinkText: 'footerPm',
+          },
+          {
+            footerBoxlink: 'footerDepartmentAgenciesURL',
+            footerBoxLinkText: 'footerDepartmentAgencies',
+          },
 
-            {
-              footerBoxlink: 'footerTreatiesURL',
-              footerBoxLinkText: 'footerTreaties',
-            },
-            {
-              footerBoxlink: 'footerHowGovWorksURL',
-              footerBoxLinkText: 'footerHowGovWorks',
-            },
-            {
-              footerBoxlink: 'footerPublicServiceURL',
-              footerBoxLinkText: 'footerPublicService',
-            },
-            {
-              footerBoxlink: 'footerGovReportingURL',
-              footerBoxLinkText: 'footerGovReporting',
-            },
-            {
-              footerBoxlink: 'footerOpenGovURL',
-              footerBoxLinkText: 'footerOpenGov',
-            },
-          ]}
-        />
-      </footer>
+          {
+            footerBoxlink: 'footerTreatiesURL',
+            footerBoxLinkText: 'footerTreaties',
+          },
+          {
+            footerBoxlink: 'footerHowGovWorksURL',
+            footerBoxLinkText: 'footerHowGovWorks',
+          },
+          {
+            footerBoxlink: 'footerPublicServiceURL',
+            footerBoxLinkText: 'footerPublicService',
+          },
+          {
+            footerBoxlink: 'footerGovReportingURL',
+            footerBoxLinkText: 'footerGovReporting',
+          },
+          {
+            footerBoxlink: 'footerOpenGovURL',
+            footerBoxLinkText: 'footerOpenGov',
+          },
+        ]}
+      />
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import DateModified from '../components/atoms/DateModified'
-import Footer from '../components/molecules/Footer'
 import styles from '../styles/Home.module.css'
 import { getLocalTopics } from './api/getData'
 
