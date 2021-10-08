543d838e676d7a9cd21b454d39436040af579383
shewood <shewood01@gmail.com>
2021-10-08 08:57:30 -0400

    button text as a prop

--- components/molecules/FeatureBlock.js
+++ components/molecules/FeatureBlock.js
@@ -1,25 +1,24 @@
 import propTypes from 'prop-types'
-import { useRouter } from 'next/router'
 import { ActionButton } from '../atoms/ActionButton'
 import Image from 'next/image'
 /**
  * Featured Content banner
  */
 export default function FeatureBlock(props) {
-  const { asPath } = useRouter()
-  // const t = props.language === 'en' ? en : fr
-
   return (
     <div
       className="flex flex-col justify-between sm:flex-row-reverse bg-light-solid "
       id="feature-block"
     >
       <div className="bg-splash-page bg-cover bg-center w-full py-32  ">
-        {/* <img
-              src="/sp-bg-1.jpg"
-              alt="Picture of something nice"
-              className="object-center"
-            ></img> */}
+        {/* <div className="aspect-w-16 aspect-h-9">
+          <Image
+            src="/sp-bg-1.jpg"
+            alt="Picture of something nice"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div> */}
       </div>
 
       <div className="p-3 grid sm:ml-7 md:ml-10 lg:ml-12 container  mx-auto md:w-3/5">
@@ -34,7 +33,7 @@ export default function FeatureBlock(props) {
             id="feature-link-button"
             href="#"
           >
-            text on button
+            {props.buttonText}
           </ActionButton>
         </div>
       </div>
@@ -45,10 +44,15 @@ export default function FeatureBlock(props) {
 FeatureBlock.propTypes = {
   // title text to be displayed
   title: propTypes.string,
+
+  // Featured content title text to be displayed
   featuredContent: propTypes.string,
 
   // body text to be displayed
   body: propTypes.string,
+
+  // button text to be displayed
+  buttonText: propTypes.string,
 
   // id of the element for testing
   id: propTypes.string.isRequired,
--- pages/home.js
+++ pages/home.js
@@ -39,6 +39,7 @@ export default function Home(props) {
         title="Featured: "
         featuredContent="Life Journeys"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, tortor vitae mattis viverra, ipsum lacus malesuada ligula, eu pharetra ipsum libero a diam."
+        buttonText="Text on button"
       ></FeatureBlock>
     </Layout>
   )
