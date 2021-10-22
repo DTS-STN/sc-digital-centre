b92aa1ad511c96f4b024f6a7e5f76a9dc5dfdadf
Thomas Dacey <DaceyTom2@gmail.com>
2021-10-22 10:20:44 -0400

    Added _document with example script

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{ __html: 'console.log("Do analytics")' }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
