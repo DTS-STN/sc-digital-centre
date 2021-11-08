cedbda4374633c31a16142e8943a37bf38765fa6
tishamIslam <tisham.islam@gmail.com>
2021-11-08 12:08:48 -0500

    updated jest test

--- components/molecules/SearchFilterForm.test.js
+++ components/molecules/SearchFilterForm.test.js
@@ -6,6 +6,25 @@ import SearchFilterForm from './SearchFilterForm'
 expect.extend(toHaveNoViolations)
 
 describe('SearchFilterForm', () => {
+  const ageRangeOptions = [
+    { key: 0, name: 'Under 18 years old' },
+    { key: 1, name: '18-100 bajillion' },
+  ]
+  const incomeOptions = [
+    { key: 0, name: 'Between $0 - $23999' },
+    { key: 1, name: 'Between $23999 - $42999' },
+    { key: 2, name: 'Between $42999 - $72999' },
+  ]
+  const eligibilityOptions = [
+    { key: 0, id: 'living-with-disability', name: 'living with a disability' },
+    {
+      key: 1,
+      id: 'caregiver-to-disability',
+      name: 'caregiver to someone with a disability',
+    },
+    { key: 2, id: 'widowed', name: 'widowed' },
+  ]
+
   it('renders SearchFilterForm in english', () => {
     render(
       <SearchFilterForm
@@ -21,9 +40,15 @@ describe('SearchFilterForm', () => {
   it('has no a11y violations', async () => {
     const { container } = render(
       <SearchFilterForm
-        filterHeader="search text"
+        ageRangeOptions={ageRangeOptions}
+        incomeOptions={incomeOptions}
+        eligibilityOptions={eligibilityOptions}
+        filterHeader="{t.filters}"
         cancelText="{t.cancel}"
         submitText="{t.submit}"
+        ageRangeLabel="{t.ageRange}"
+        incomeLabel="{t.annualIncome}"
+        eligibilityLabel="{t.eligibility}"
       />
     )
     const results = await axe(container)
