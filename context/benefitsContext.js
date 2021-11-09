f41d1332be402160c4f3ca6b6d7147cb0a2fc4fe
MarcoGoC <marcogoc951@gmail.com>
2021-11-09 13:57:32 -0500

    saving changes

--- context/benefitsContext.js
+++ context/benefitsContext.js
@@ -6,26 +6,26 @@ export const BenefitsContext = createContext()
 const BenefitsProvider = ({ children }) => {
   const [benefits, setBenefits] = useState([])
 
-  // We could do the fetch here, example.
-  const getBenefits = async () => {
-    try {
-      // The path will need to change
-      const res = await getAEMElements('benefits/oas.json')
-      if (!res.error) {
-        setBenefits(res)
-      }
-    } catch (err) {
-      console.error(err)
-    }
-  }
+  // // We could do the fetch here, example.
+  // const getBenefits = async () => {
+  //   try {
+  //     // The path will need to change
+  //     const res = await getAEMElements('benefits/oas.json')
+  //     if (!res.error) {
+  //       setBenefits(res)
+  //     }
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+  // }
 
-  // Fetch benefits and services on first load
-  getBenefits()
+  // // Fetch benefits and services on first load
+  // getBenefits()
   // List of functions that will be returned
   const contextValues = {
     benefits,
     setBenefits,
-    getBenefits,
+    // getBenefits,
   }
 
   return (
--- pages/benefits/[id].js
+++ pages/benefits/[id].js
@@ -1,39 +1,54 @@
 import Layout from '../../components/organisms/Layout'
 import { getAEMElements, getAEMFragments } from '../api/getData'
+import { useContext } from 'react'
+import { BenefitsContext } from '../../context/benefitsContext'
+
 import en from '../../locales/en'
 import fr from '../../locales/fr'
+
 export default function BenefitPage(props) {
+  //
+  console.log('props : ', props)
+
   const t = props.locale === 'en' ? en : fr
+
+  const { benefits } = useContext(BenefitsContext)
+
+  console.log(benefits)
+
   return (
-    <Layout locale={props.locale} title={props.params.id}>
-      ... this ia a page
+    <Layout locale={props.locale} title={props.benefit.id}>
+      <h1 className="font-extrabold text-red-800 text-3xl text-center">
+        {props.benefit.id}
+      </h1>
+
+      <p className="text-center my-24"> ... this ia a page</p>
     </Layout>
   )
 }
+
 export async function getStaticPaths() {
-  let features = await getAEMElements('benefits/oas.json')
+  //let features = await getAEMElements('benefits/oas.json')
   return {
     paths: [
       {
         params: {
-          id: features.elements.scPageNameFr.value,
+          id: 'OAS',
         },
       },
     ],
     fallback: false,
   }
 }
-export async function getStaticProps({ locale, params }) {
+
+export async function getStaticProps(context) {
   // Here we can fetch the chosen benefits with params.id before returning it as a prop.
+  console.log('context = ', context)
+
   return {
     props: {
-      locale,
-      params,
+      locale: context.locale,
+      benefit: context.params,
     },
   }
 }
-//   export async function getStaticPaths() {
-//     scPageNameEn: [Object],
-//     scPageNameFr: [Object],
-//     // Return a list of possible value for id
-//   }
