509f452fd2408dd2be55fd6a9eabd68ce2b72636
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-11-09 13:04:54 -0500

    Wroking on the branch.

--- context/benefitsContext.js
+++ context/benefitsContext.js
@@ -1,4 +1,5 @@
 import { useState, createContext } from 'react'
+import { getAEMElements } from '../pages/api/getData'
 
 export const BenefitsContext = createContext()
 
@@ -6,21 +7,25 @@ const BenefitsProvider = ({ children }) => {
   const [benefits, setBenefits] = useState([])
 
   // We could do the fetch here, example.
-  // const getBenefits = async () => {
-  //   try {
-  //     const res = await fetch()
-  //     const allBenefits = await res.json()
-  //     setBenefits(allBenefits)
-  //   } catch (err) {
-  //     console.error(err)
-  //   }
-  // };
+  const getBenefits = async () => {
+    try {
+      // The path will need to change
+      const res = await getAEMElements('benefits/oas.json')
+      if (!res.error) {
+        setBenefits(res)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
+  // Fetch benefits and services on first load
+  getBenefits()
   // List of functions that will be returned
   const contextValues = {
     benefits,
     setBenefits,
-    //getBenefits,
+    getBenefits,
   }
 
   return (
--- pages/benefits/[id].js
+++ pages/benefits/[id].js
@@ -1,61 +1,39 @@
-import Layout from '../components/organisms/Layout'
-import { getAEMElements, getAEMFragments } from './api/getData'
-
-import en from '../locales/en'
-import fr from '../locales/fr'
-
-export default function BenefitPage() {
+import Layout from '../../components/organisms/Layout'
+import { getAEMElements, getAEMFragments } from '../api/getData'
+import en from '../../locales/en'
+import fr from '../../locales/fr'
+export default function BenefitPage(props) {
   const t = props.locale === 'en' ? en : fr
-
   return (
-    <Layout locale={props.locale} title="benefit">
+    <Layout locale={props.locale} title={props.params.id}>
       ... this ia a page
     </Layout>
   )
 }
-
 export async function getStaticPaths() {
-  const paths = getAEMElementIds()
+  let features = await getAEMElements('benefits/oas.json')
   return {
-    paths,
+    paths: [
+      {
+        params: {
+          id: features.elements.scPageNameFr.value,
+        },
+      },
+    ],
     fallback: false,
   }
 }
-
-export async function getStaticProps({ locale }) {
-  let benefits = []
-  let errorCode = false
-  let featured = []
-
-  //
-  // IF content enabled get the data from the api
-  //
-
-  let features = await getAEMElements('benefits/oas.json')
-  errorCode = features.error
-  if (features.elements && !errorCode) {
-    featured = features.elements
-  }
-
-  let AEMbenefits = await getAEMFragments('benefits.json')
-  errorCode = AEMbenefits.error
-  if (AEMbenefits.apiData && !errorCode) {
-    benefits = AEMbenefits.apiData.entities
-  }
-
+export async function getStaticProps({ locale, params }) {
+  // Here we can fetch the chosen benefits with params.id before returning it as a prop.
   return {
     props: {
-      benefits,
-      errorCode,
       locale,
-      featured,
+      params,
     },
   }
 }
-
 //   export async function getStaticPaths() {
 //     scPageNameEn: [Object],
 //     scPageNameFr: [Object],
-
 //     // Return a list of possible value for id
 //   }
