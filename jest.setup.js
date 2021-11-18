afe778ef4681886f7fff89e1d9d0c7ec06bef8cb
Zain Syed <zeddotes@gmail.com>
2021-11-18 10:10:43 -0500

    - updated test setup file for failing .env propagation
    Signed-off-by: Zain Syed <zeddotes@gmail.com>

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,11 @@
 import { loadEnvConfig } from '@next/env'
 
+process.env.NEXT_PUBLIC_BUILD_DATE = new Date().toLocaleDateString('en-CA')
+process.env.NEXT_CONTENT_API =
+  'https://www.canada.ca/api/assets/decd-endc/content-fragments/'
+process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL =
+  '//assets.adobedtm.com/be5dfd287373/0127575cd23a/launch-913b1beddf7a-staging.min.js'
+
 export default async () => {
   loadEnvConfig(process.env.PWD)
 }
