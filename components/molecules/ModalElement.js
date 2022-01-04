92eb8c492f8de55113208fea2f54b83f6d4181a6
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2022-01-04 14:03:12 -0500

    fixing error message

--- components/molecules/ModalElement.js
+++ components/molecules/ModalElement.js
@@ -7,8 +7,8 @@ export default function ModalElement(props) {
   }
 
   const options = props.options ?? {
-    clickOutsideDeactivates: 'true',
-    preventScroll: 'true',
+    clickOutsideDeactivates: false,
+    preventScroll: true,
   }
 
   return (
