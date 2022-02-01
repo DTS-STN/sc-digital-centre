b1cf79bc2550a0dbdd2e682241dd03f4c40d3565
nhamade <nhamade@gmail.com>
2022-01-31 19:21:58 -0500

    add css to circle progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/fonts.css'
 import '../styles/stepper.css'
+import '../styles/circle-progess-bar.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
