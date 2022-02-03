3cbba87edae96c1c6bac3094b9ff31ba71c93b25
arsani <arsanig@users.noreply.github.com>
2022-02-03 15:58:08 -0500

    Resolving npm issues and refactoring CircleProgess to use Tailwind colors

--- components/atoms/CircleProgressBar.js
+++ components/atoms/CircleProgressBar.js
@@ -1,32 +1,32 @@
 import ProgressBar from 'react-customizable-progressbar'
-
-import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config'
 
-export class CircleProgressBar extends Component {
-  render() {
-    return (
-      <div className="absolute inset-x-6 top-9 h-16">
-        <ProgressBar
-          progress={this.props.progress}
-          steps={this.props.steps}
-          radius={28}
-          strokeColor={'#259F46'}
-          strokeWidth={4}
-          trackStrokeWidth={4}
-        >
-          <div className="relative">
-            <p className="px-9 -py-20 absolute -top-24 h-24 grid content-center text-green-600 font-bold font-display text-lg">
-              {this.props.progress}
-            </p>
-          </div>
-        </ProgressBar>
-      </div>
-    )
-  }
+export default function CircleProgressBar(props) {
+  const fullConfig = resolveConfig(tailwindConfig)
+
+  return (
+    <div className="absolute inset-x-6 top-9 h-16">
+      <ProgressBar
+        progress={props.progress}
+        steps={props.steps}
+        radius={28}
+        strokeColor={fullConfig.theme.colors.green['active']}
+        strokeWidth={4}
+        trackStrokeWidth={4}
+      >
+        <div className="relative">
+          <p className="px-9 -py-20 absolute -top-24 h-24 grid content-center text-green-active font-bold font-display text-lg">
+            {props.progress}
+          </p>
+        </div>
+      </ProgressBar>
+    </div>
+  )
 }
+
 CircleProgressBar.propTypes = {
   progress: PropTypes.number,
   steps: PropTypes.number,
 }
-export default CircleProgressBar
