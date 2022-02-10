5f11c506607a7eee1bbe89bc9e167ed730815540
nhamade <nhamade@gmail.com>
2022-02-10 14:12:55 -0500

    clean up

--- components/atoms/CircleProgressBar.test.js
+++ components/atoms/CircleProgressBar.test.js
@@ -13,20 +13,12 @@ expect.extend(toHaveNoViolations)
 
 describe('CircleProgressBar', () => {
   it('renders CircleProgressBar', () => {
-    useRouter.mockImplementation(() => ({
-      pathname: '/',
-      asPath: '/',
-    }))
     render(<CircleProgressBar progress={11} steps={22} />)
     const progressText = screen.getByText('11')
     expect(progressText).toBeInTheDocument()
   })
 
   it('has no a11y violations', async () => {
-    useRouter.mockImplementation(() => ({
-      pathname: '/',
-      asPath: '/',
-    }))
     const { container } = render(<CircleProgressBar progress={11} steps={22} />)
     const results = await axe(container)
     expect(results).toHaveNoViolations()
