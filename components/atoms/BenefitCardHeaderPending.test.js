839d956ff633cbc448c32b7c149aa9238266a66a
nabil386 <nhamade@gmail.com>
2022-03-14 14:09:57 -0400

    Merge dashboard-sprint10 into dashboard
    * Creating the CPPD Active Card
    
    * Adding CPPD Pending Card
    
    Co-authored-by: amirpourpanahi <51453299+amirpourpanahi@users.noreply.github.com>

--- components/atoms/BenefitCardHeaderPending.test.js
+++ components/atoms/BenefitCardHeaderPending.test.js
@@ -45,6 +45,48 @@ const pendingCPP = {
   ],
 }
 
+const pendingCPPD = {
+  benefitType: 'CPPD',
+  benefitName: 'Canada Pension Plan Disability',
+  status: 'Pending',
+  statusDescription:
+    'Your application is pending, we will notify you with decision',
+  applicationDate: 'July 11, 2021',
+  estimatedDateOfDecision: 'July 31, 2021',
+  applicationType: 'Canada Pension Plan (CPP)',
+  applicationDescription: 'Online application received',
+  nextPaymentAmount: 735.34,
+  nextPaymentDate: 'September 30, 2021',
+  pensionStartDate: 'August 1, 2021',
+  payeeFullName: 'John Smith Doe',
+  payeeAddress: '123 - 00 Fake Street, Ottawa, On A1A-1A1',
+  accountNumber: 'XXXX-123',
+  institutionNumber: '002',
+  payerName: 'Government of Canada',
+  paymentDepositDate: 'August 30, 2021',
+  paymentStatus: 'Issued',
+  paymentType: 'Direct Deposit',
+  additionalInformation:
+    'We will notify you as soon as we have process your application.',
+
+  applicationStatus: 'Application Submitted',
+  pendingBenefits: 'Retirement pension',
+  lastUpdates: [
+    {
+      label: 'Documents reviewed',
+      description: 'September 8, 2021',
+    },
+    {
+      label: 'Documents uploaded',
+      description: 'September 6, 2021',
+    },
+    {
+      label: 'Documents requested',
+      description: 'September 5, 2021',
+    },
+  ],
+}
+
 describe('BenefitCardHeaderPending Tests', () => {
   it('renders the Header of a Pending Benefit', () => {
     render(<BenefitCardHeaderPending benefit={pendingCPP} locale={'en'} />)
@@ -54,6 +96,16 @@ describe('BenefitCardHeaderPending Tests', () => {
     expect(titleText).toBeInTheDocument()
     expect(applicationStatus).toBeInTheDocument()
     expect(submittedOn).toBeInTheDocument()
+  })
+
+  it('renders the Header of a CPPD', () => {
+    render(<BenefitCardHeaderPending benefit={pendingCPPD} locale={'en'} />)
+    const titleTextCPPD = screen.getByText('Canada Pension Plan')
+    expect(titleTextCPPD).toBeInTheDocument()
+    const titleTextDisability = screen.queryByText(
+      'Canada Pension Plan Disability'
+    )
+    expect(titleTextDisability).toBeNull()
   })
 
   it('has no a11y violations', async () => {
