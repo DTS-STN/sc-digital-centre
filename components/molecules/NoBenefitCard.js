c6e48a37cbf123c87c16d332b1f514b93830290a
amirpourpanahi <51453299+amirpourpanahi@users.noreply.github.com>
2022-03-28 13:39:25 -0400

    No Benefit Card - CPP (#248)
    * Inactive Benefits cards (EI & CPPD)
    
    * Pending cards changed to gray
    
    * No Benefit Card - CPP
    
    * Update snapshots

---
+++ components/molecules/NoBenefitCard.js
@@ -0,0 +1,66 @@
+import React, { useState, useRef } from 'react'
+import PropTypes from 'prop-types'
+import NoBenefitCardHeader from '../atoms/NoBenefitCardHeader'
+import BenefitTasks from './BenefitTasks'
+import en from '../../locales/en'
+import fr from '../../locales/fr'
+
+const NoBenefitCard = (props) => {
+  const t = props.locale === 'en' ? en : fr
+  const [isOpen, setIsOpen] = useState(false)
+  const [btnCaption, setBtnCaption] = useState(t.viewMore)
+  const topOfCardRef = useRef(null)
+  const topOfTaskRef = useRef(null)
+  const handleClick = () => {
+    setBtnCaption(isOpen ? t.viewMore : t.viewLess)
+    setIsOpen(!isOpen)
+  }
+
+  const scrollTo = () => {
+    if (!topOfTaskRef.current || !topOfCardRef.current) return
+    isOpen
+      ? topOfCardRef.current.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        })
+      : topOfTaskRef.current.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        })
+  }
+
+  return (
+    <div className="benefit-card" ref={topOfCardRef}>
+      {/* NoBenefit Card Header */}
+      <NoBenefitCardHeader benefit={props.benefit} locale={props.locale} />
+
+      {/* Top tasks */}
+      <div ref={topOfTaskRef}>
+        <BenefitTasks
+          benefitType={props.benefit.benefitType}
+          isExpanded={isOpen}
+          tasks={props.tasks}
+          locale={props.locale}
+          noBenefit={true}
+        />
+      </div>
+
+      <button
+        onClick={() => {
+          handleClick()
+          scrollTo()
+        }}
+        className="pl-5 py-5 sm:pl-10"
+      >
+        {btnCaption}
+      </button>
+    </div>
+  )
+}
+export default NoBenefitCard
+
+NoBenefitCard.propTypes = {
+  benefit: PropTypes.shape({
+    benefitType: PropTypes.oneOf(['CPP', 'OAS', 'GIS', 'EI', 'CPPD']),
+  }),
+}
