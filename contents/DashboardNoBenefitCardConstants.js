d56069705ce909d73c9be3e0ac3f9eaeb2f8d4e7
amirpourpanahi <51453299+amirpourpanahi@users.noreply.github.com>
2022-03-29 15:58:58 -0400

    Feature/dcwc1461 No-Benefit card EI (#253)
    * NoBenefitCard - EI
    
    * Fixing NoBenefitCard className

--- contents/DashboardNoBenefitCardConstants.js
+++ contents/DashboardNoBenefitCardConstants.js
@@ -5,6 +5,12 @@ const NO_BENEFIT_CPP = {
   learnMoreLink: '',
 }
 
+const NO_BENEFIT_EI = {
+  benefitType: BenefitCode.ei,
+  learnMoreLink: '',
+}
+
 module.exports = {
   NO_BENEFIT_CPP,
+  NO_BENEFIT_EI,
 }
