edf7387d023a6e0cd4f2e41df9ec4c9377ed7247
shewood <shewood01@gmail.com>
2022-04-26 10:50:19 -0400

    removed the component

--- components/molecules/Greeting.js
+++ components/molecules/Greeting.js
@@ -1,4 +1,5 @@
 import InfoMessage from '../atoms/InfoMessage'
+import { Message } from '@dts-stn/decd-design-system'
 import en from '../../locales/en'
 import fr from '../../locales/fr'
 
@@ -11,15 +12,14 @@ export default function Greeting(props) {
         <div className="py-4 text-4xl font-bold">{`${t.greeting} ${props.name}`}</div>
         <div className="py-4 text-xl">{t.welcome}</div>
       </div>
-      <InfoMessage
+      <Message
         type="success"
         id="success"
-        alertIconAltText={t.alertIconAltText_success}
-        alertIconId={t.alertIconId_success}
-        messageHeading={t.messageHeading}
-        messageBody={t.messageBody}
+        alert_icon_alt_text={t.alertIconAltText_success}
+        alert_icon_id={t.alertIconId_success}
+        message_heading={t.messageHeading}
+        message_body={t.messageBody}
       />
-
       <div className="text-3xl font-bold mt-10">{t.myBenefitsAndServices}</div>
     </div>
   )
