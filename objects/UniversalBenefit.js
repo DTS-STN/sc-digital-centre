a035914e9beb7a5defbfccfb61d282e2191926e5
s-laugh <22419216+s-laugh@users.noreply.github.com>
2022-04-27 15:51:06 -0400

    adding api result object for context to map in comments

--- objects/UniversalBenefit.js
+++ objects/UniversalBenefit.js
@@ -1,11 +1,32 @@
 ///EI map
+// {
+//     "claimStatusCode": 3433, // status
+//     "enmBenefitType": 1, //active benefit type
+//     "messageData": "Important message", //latest update
+//     "messageId": 32, //latest update
+//     "publishDate": "2021-02-21", //latest update
+//     "nextRptDueDate": "2023-04-24", //next report due
+//     "programCode": 4543, //program name
+//     "netAmount": 32.21 //payment amount
+// }
 ///programCode = programCode
 ///statusCode = claimStatusCode
 ///typeCode = enmBenefitType
 ///summaries = [ { PaymentAmount, netAmount },
 ///              { NextReport, nextRptDueDate },
 ///              { LatestStatus, publishDate, messageData } ]
+
 ///CPP map
+// {
+//     "programCode": 32294, //status
+//     "benefitCode": 30320, //program name
+//     "benefitType": "Beneficial", //active type
+//     "benefitStatus": "Active", //latest update XXX
+//     "lastPaymentDate": "2021-02-21", //last day paid
+//     "finalPaymentDate": "2024-02-13", //last payment (future)
+//     "netAmount": 30.32, // payment amount
+//     "paymentProcessType": "Monthly" //process type
+// }
 ///programCode = benefitCode
 ///statusCode = programCode
 ///typeCode = benefitType
