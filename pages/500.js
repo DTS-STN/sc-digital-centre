e09ae4c62b308bafc02613dc29a9de5226b80bfc
s-laugh <22419216+s-laugh@users.noreply.github.com>
2022-05-04 09:21:57 -0400

    add locale back

--- pages/500.js
+++ pages/500.js
@@ -19,6 +19,6 @@ export default function Error500(props) {
 export async function getStaticProps({ locale }) {
   const langToggleLink = locale === 'en' ? '/fr/500' : '/500'
   return {
-    props: { langToggleLink },
+    props: { locale, langToggleLink },
   }
 }
