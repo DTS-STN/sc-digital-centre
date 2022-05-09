ce97aa7b3e5d6a0959944df5c10efa551ced37b1
shewood <shewood01@gmail.com>
2022-05-09 12:12:54 -0400

    removed extra div

--- components/molecules/BenefitCardHeaderSummary.js
+++ components/molecules/BenefitCardHeaderSummary.js
@@ -7,20 +7,18 @@ export default function BenefitCardHeaderSummary(props) {
   const typesWithLinks = [SummaryTypes.PaymentAmount, SummaryTypes.LatestStatus]
 
   return (
-    <div className="w-full ">
-      <div className="w-full  ">
-        <p className="">{t[props.summary.type].title}</p>
-        <p className="font-bold ">{props.summary.value}</p>
-        <p className="font-bold ">{props.summary.status ?? null}</p>
-        {!typesWithLinks.find((t) => t === props.summary.type) ? null : (
-          <p className="">
-            <Link
-              text={t[props.summary.type].linkText}
-              href={t[props.summary.type].link}
-            />
-          </p>
-        )}
-      </div>
+    <div className=" w-full">
+      <p className="">{t[props.summary.type].title}</p>
+      <p className="font-bold ">{props.summary.value}</p>
+      <p className="font-bold ">{props.summary.status ?? null}</p>
+      {!typesWithLinks.find((t) => t === props.summary.type) ? null : (
+        <p className="font-body">
+          <Link
+            text={t[props.summary.type].linkText}
+            href={t[props.summary.type].link}
+          />
+        </p>
+      )}
     </div>
   )
 }
