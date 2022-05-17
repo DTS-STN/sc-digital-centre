9734eb122d7db5220cc23e963fe210abb5556e7a
Thomas Dacey <daceytom2@gmail.com>
2022-05-17 08:57:49 -0400

    auth api calls middleware

---
+++ pages/api/programData/_middleware.js
@@ -0,0 +1,17 @@
+import { getSession } from 'next-auth/react'
+import { NextResponse } from 'next/server'
+
+export async function middleware(req, ev) {
+  if (
+    !process.env.AUTH_DISABLED ||
+    process.env.AUTH_DISABLED.toLowerCase() === 'false'
+  ) {
+    const session = await getSession({ req })
+
+    if (!session) {
+      return new Response('Auth required', {
+        status: 401,
+      })
+    }
+  }
+}
