582c078ffb728b94b72b9f2531c8b29fea371235
Thomas Dacey <daceytom2@gmail.com>
2022-05-18 11:41:05 -0400

    cleaned up benefits loading

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,19 +4,10 @@ import BenefitApplicationCard from '../components/molecules/BenefitApplicationCa
 import Greeting from '../components/molecules/Greeting'
 import { getBenefitCards } from '../contents/BenefitCards'
 import { getNoBenefitCards } from '../contents/NoBenefitCards'
-import {
-  APPLICATION_CARD_OAS,
-  APPLICATION_CARD_GIS,
-  APPLICATION_CARD_CPPD,
-  APPLICATION_CARD_CPP,
-  getAdvertsingCards,
-} from '../contents/BenefitAdvertisingCards'
+import { getAdvertsingCards } from '../contents/BenefitAdvertisingCards'
 import { getSession } from 'next-auth/react'
 import UniversalBenefitCard from '../components/molecules/UniversalBenefitCard'
-import { StatusCodes } from '../constants/StatusCodes'
-import { ProgramCodes } from '../constants/ProgramCodes'
-import { TypeCodes } from '../constants/ProgramTypeCodes'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Dashboard(props) {
   const [benefitCards, setBenefitCards] = useState(props.benefitCards)
@@ -24,74 +15,19 @@ export default function Dashboard(props) {
     props.advertisingCards
   )
   const [noBenefitCards, setNoBenefitCards] = useState(props.noBenefitCards)
-  const [usersBenefits, setUsersBenefits] = useState([])
+  const [cppBenefit, setCppBenefit] = useState()
+  const [eiBenefit, setEiBenefit] = useState()
   const [cppLoaded, setCppLoaded] = useState(false)
   const [eiLoaded, setEiLoaded] = useState(false)
   //no requirements to do anything with the errors yet
   const [cppError, setCppError] = useState(false)
   const [eiError, setEiError] = useState(false)
 
-  const FilterAdvertisingCards = useCallback(
-    (benefits) => {
-      //determine which advertising cards to remove
-      const cardsToRemove = []
-      if (benefits && benefits.length) {
-        if (
-          !CheckAllBenefitsForAdvertising(
-            benefits,
-            [StatusCodes.inPayment, StatusCodes.Pending],
-            ProgramCodes.CPP,
-            TypeCodes.CPPRetirement
-          )
-        ) {
-          cardsToRemove.push(APPLICATION_CARD_CPP)
-        }
-        if (
-          !CheckAllBenefitsForAdvertising(
-            benefits,
-            [StatusCodes.inPayment, StatusCodes.Pending],
-            ProgramCodes.OAS
-          )
-        ) {
-          cardsToRemove.push(APPLICATION_CARD_OAS)
-        }
-        if (
-          !CheckAllBenefitsForAdvertising(
-            benefits,
-            [StatusCodes.inPayment, StatusCodes.Pending],
-            ProgramCodes.GIS
-          )
-        ) {
-          cardsToRemove.push(APPLICATION_CARD_GIS)
-        }
-        if (
-          !CheckAllBenefitsForAdvertising(
-            benefits,
-            [StatusCodes.inPayment, StatusCodes.Pending],
-            ProgramCodes.CPP,
-            TypeCodes.CPPDisability
-          )
-        ) {
-          cardsToRemove.push(APPLICATION_CARD_CPPD)
-        }
-      }
-
-      //now remove them if they should be removed
-      return cardsToRemove.length
-        ? props.advertisingCards.filter((c) =>
-            cardsToRemove.find((r) => r.benefitType !== c.benefitType)
-          )
-        : props.advertisingCards
-    },
-    [props.advertisingCards]
-  )
-
   useEffect(() => {
     fetch(`/api/programData/cpp`)
       .then((res) => res.json())
       .then((data) => {
-        usersBenefits.push(data)
-        setAdvertisingCards(FilterAdvertisingCards(usersBenefits))
+        setCppBenefit(data)
       })
       .catch((error) => {
         setCppError(error)
@@ -101,8 +37,7 @@ export default function Dashboard(props) {
     fetch(`/api/programData/ei`)
       .then((res) => res.json())
       .then((data) => {
-        usersBenefits.push(data)
-        setAdvertisingCards(FilterAdvertisingCards(usersBenefits))
+        setEiBenefit(data)
       })
       .catch((error) => {
         setEiError(error)
@@ -114,19 +49,23 @@ export default function Dashboard(props) {
     <>
       <Greeting locale={props.locale} name="Mary" />
 
-      {/* todo, design to create loading */}
-      {cppLoaded && eiLoaded ? null : 'Loading User Benefit Data...'}
-      {!usersBenefits || !usersBenefits.length
-        ? null
-        : usersBenefits.map((value, index) => {
-            return (
-              <UniversalBenefitCard
-                key={index}
-                locale={props.locale}
-                benefit={value}
-              />
-            )
-          })}
+      {cppLoaded ? null : 'Loading User Benefit Data...'}
+      {cppBenefit ? (
+        <UniversalBenefitCard
+          key={0}
+          locale={props.locale}
+          benefit={cppBenefit}
+        />
+      ) : null}
+
+      {eiLoaded ? null : 'Loading User Benefit Data...'}
+      {eiBenefit ? (
+        <UniversalBenefitCard
+          key={1}
+          locale={props.locale}
+          benefit={eiBenefit}
+        />
+      ) : null}
 
       {/* Old Benefit Cards, to be removed once mocks are generated with new cards */}
       {benefitCards.map((value, index) => {
@@ -135,6 +74,13 @@ export default function Dashboard(props) {
 
       {/* application or "advertising" cards */}
       {advertisingCards.map((value, index) => {
+        if (value.benefitType === 'CPP' && cppBenefit) {
+          console.log(value)
+          console.log(cppBenefit)
+          return
+        } else if (value.benefitType === 'EI' && eiBenefit) {
+          return
+        }
         return (
           <div key={index}>
             <BenefitApplicationCard
@@ -191,26 +137,4 @@ export async function getServerSideProps({ req, locale }) {
       metadata,
     },
   }
-}
-
-/// return: the benefit can be advertised
-/// benefit: the benefit to be checked
-/// program: the program to match to
-/// type: the type to match to
-/// status: status' that the advertising card can't be shown with
-function CheckAllBenefitsForAdvertising(benefits, status, program, type) {
-  var matchingBenefit
-  if (typeof type === 'undefined')
-    //overloaded function handling
-    matchingBenefit = benefits.find((b) => b.programCode == program)
-  else
-    matchingBenefit = benefits.find(
-      (b) => b.programCode == program && b.typeCode == type
-    )
-
-  if (matchingBenefit == null) return true
-  //check if there is a status to evaluate
-  //if (typeof status === 'undefined' || status.length == 0) return false
-  //evaluate for a matching status
-  return !status.find((s) => s == matchingBenefit.statusCode)
 }
