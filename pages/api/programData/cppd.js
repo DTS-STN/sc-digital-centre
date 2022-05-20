e8bff239677495ffbb16bb33361a35056d7bff01
Thomas Dacey <daceytom2@gmail.com>
2022-05-20 14:39:42 -0400

    mock file for cppd

--- pages/api/programData/cppd.js
+++ pages/api/programData/cppd.js
@@ -1,19 +1,12 @@
+import mockCppdInPayment from '../../../contents/mockdata/CppdInPayment.json'
 import { ACTIVE_CPPD_TASKS } from '../../../contents/DashboardBenefitTasksConstants'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    res.status(200).json({
-      programCode: 'cppd',
-      statusCode: 'inPayment',
-      typeCode: 'CPPBeneficial',
-      summaries: [
-        { type: 'PaymentAmount', value: 612.94 },
-        { type: 'NextPayment', value: new Date('2021-09-30T00:00:00') },
-        { type: 'LatestStatus', value: new Date('2021-07-15T00:00:00') },
-      ],
-      taskGroups: [{ Header: 'paymentTasks', Tasks: ACTIVE_CPPD_TASKS }],
-      taskHeadingKey: 'paymentsTaxesAccount',
-    })
+    ;(mockCppdInPayment.taskGroups = [
+      { Header: 'paymentTasks', Tasks: ACTIVE_CPPD_TASKS },
+    ]),
+      res.status(200).json(mockCppdInPayment)
   } else {
     res.status(405)
   }
