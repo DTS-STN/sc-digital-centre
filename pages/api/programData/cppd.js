9fc9502dc4d2e2ffdad84fa82a24ee1a5f46ced8
Thomas Dacey <daceytom2@gmail.com>
2022-05-26 08:02:34 -0400

    removed cppd mock

--- pages/api/programData/cppd.js
+++ pages/api/programData/cppd.js
@@ -1,12 +1,31 @@
-import mockCppdInPayment from '../../../contents/mockdata/CppdInPayment.json'
 import { ACTIVE_CPPD_TASKS } from '../../../contents/DashboardBenefitTasksConstants'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    ;(mockCppdInPayment.taskGroups = [
-      { Header: 'paymentTasks', Tasks: ACTIVE_CPPD_TASKS },
-    ]),
-      res.status(200).json(mockCppdInPayment)
+    res.status(200).json({
+      programCode: 'cppd',
+      statusCode: 'inPayment',
+      typeCode: 'CPPBeneficial',
+      summaries: [
+        { type: 'PaymentAmount', value: '612.94' },
+        { type: 'NextPayment', value: '2021-09-30T00:00:00.000Z' },
+        { type: 'LatestStatus', value: '2021-07-15T00:00:00.000Z' },
+      ],
+      taskGroups: [
+        {
+          Header: 'paymentTasks',
+          Tasks: [
+            {
+              task: 'allPaymentsTask',
+              taskIcon: '/images/dashboard/oas-payment-icon.svg',
+              taskLink: 'allPaymentsTaskLink',
+            },
+          ],
+        },
+      ],
+      taskGroups: [{ Header: 'paymentTasks', Tasks: ACTIVE_CPPD_TASKS }],
+      taskHeadingKey: 'paymentsTaxesAccount',
+    })
   } else {
     res.status(405)
   }
