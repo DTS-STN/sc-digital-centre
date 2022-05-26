e5cc618400aac35ca3be0dd89a3f08cc1133d41f
s-laugh <22419216+s-laugh@users.noreply.github.com>
2022-05-26 15:02:21 -0400

    converting mock data to mock interop result

--- mockdata/MockData.js
+++ mockdata/MockData.js
@@ -2,8 +2,12 @@ import CPPDUser1 from './user1/cppd.json'
 import CPPDUser2 from './user2/cppd.json'
 import EIUser1 from './user1/ei.json'
 import EIUser2 from './user2/ei.json'
+import CPPDefaultUser from './userDefault/cpp.json'
 
 export const mockData = {
+  default: {
+    CPP: CPPDefaultUser,
+  },
   1: {
     CPPD: CPPDUser1,
     EI: EIUser1,
