60db920772e61819f74a5d07e6b49d28ecb18392
shewood <shewood01@gmail.com>
2022-06-06 10:47:59 -0400

    scenario 4

--- mockdata/MockData.js
+++ mockdata/MockData.js
@@ -1,6 +1,8 @@
 import EIUser1 from './user1/ei.json'
 import EIUser2 from './user2/ei.json'
 import CPPUser3 from './user3/cpp.json'
+import CPPUser4 from './user4/cpp.json'
+import OASUser4 from './user4/oas.json'
 import CPPDefaultUser from './userDefault/cpp.json'
 import OASDefaultUser from './userDefault/oas.json'
 import CPPDDefaultUser from './userDefault/cppd.json'
@@ -28,6 +30,8 @@ export const mockData = {
   4: {
     CPPD: undefined,
     EI: undefined,
+    CPP: CPPUser4,
+    OAS: OASUser4,
   },
   5: {
     CPPD: undefined,
