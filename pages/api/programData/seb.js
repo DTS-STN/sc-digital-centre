9dc1069cc6857fb333f76c37eb7e04712411634f
s-laugh <22419216+s-laugh@users.noreply.github.com>
2022-06-06 11:09:21 -0400

    adding seb with new common function

--- pages/api/programData/seb.js
+++ pages/api/programData/seb.js
@@ -1,29 +1,12 @@
-import { getCookie } from 'cookies-next'
 import { MapSEBCard } from '../../../lib/BenefitsMapping'
-import { mockData } from '../../../mockdata/MockData'
+import { FetchProgramData } from './_middleware'
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    //get data
-    let userData
-    const userid = getCookie('userid', { req, res })
-    if (userid) {
-      //Mock userid response
-      userData = mockData[userid].SEB
-    } else {
-      //Mock interop response
-      //return res.status(501) //errors not properly handled in client
-    }
-
-    //format data
-    if (userData) {
-      const benefits = MapSEBCard(userData)
-      res.status(200).json(benefits)
-    } else {
-      res.status(204).end()
-    }
-  } else {
-    res.status(405)
-  }
-  return res
+  return FetchProgramData(
+    req,
+    res,
+    null,
+    (result) => MapSEBCard(result),
+    (mockDataSet) => mockDataSet.SEB
+  )
 }
