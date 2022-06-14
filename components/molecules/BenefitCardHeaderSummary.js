c87ae8789008c1a102a104de27bb980390a75e14
Thomas Dacey <daceytom2@gmail.com>
2022-06-14 15:35:53 -0400

    added logic to universal benefit card

--- components/molecules/BenefitCardHeaderSummary.js
+++ components/molecules/BenefitCardHeaderSummary.js
@@ -4,23 +4,23 @@ export default function BenefitCardHeaderSummary(props) {
   return (
     <li className="w-full">
       {/* Title */}
-      <p className="font-bold">{props.summary.title}</p>
+      <p className="font-bold">{props.title}</p>
 
       {/* Optional Status */}
-      {props.summary.status ? (
-        <p className={props.summary.statusClassName}>{props.summary.status}</p>
+      {props.status ? (
+        <p className={props.statusClassName}>{props.status}</p>
       ) : null}
 
       {/* Summary */}
-      <p className={props.summary.valueClassName}>{props.summary.value}</p>
+      <p className={props.valueClassName}>{props.value}</p>
 
       {/* Optional Link */}
-      {props.summary.link ? (
+      {props.link ? (
         <a
-          href={props.summary.link}
+          href={props.link}
           className="mt-1 text-link-blue-default underline text-base hover:text-link-blue-hover"
         >
-          {props.summary.linkText}
+          {props.linkText}
         </a>
       ) : null}
     </li>
@@ -28,14 +28,11 @@ export default function BenefitCardHeaderSummary(props) {
 }
 
 BenefitCardHeaderSummary.propTypes = {
-  summary: propTypes.shape({
-    type: propTypes.string.isRequired,
-    title: propTypes.string.isRequired,
-    status: propTypes.string,
-    statusClassName: propTypes.string,
-    value: propTypes.any.isRequired,
-    valueClassName: propTypes.string,
-    link: propTypes.string,
-    linkText: propTypes.string,
-  }),
+  title: propTypes.string.isRequired,
+  status: propTypes.string,
+  statusClassName: propTypes.string,
+  value: propTypes.any.isRequired,
+  valueClassName: propTypes.string,
+  link: propTypes.string,
+  linkText: propTypes.string,
 }
