469dd0da57f0e9bc471b759690973379f081f706
s-laugh <22419216+s-laugh@users.noreply.github.com>
2022-06-17 13:39:21 -0400

    add profile tests for getStaticProps

--- __tests__/pages/profile.test.js
+++ __tests__/pages/profile.test.js
@@ -1,8 +1,7 @@
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { axe, toHaveNoViolations } from 'jest-axe'
-
-import Profile from '../../pages/profile'
+import Profile, { getStaticProps } from '../../pages/profile'
 
 expect.extend(toHaveNoViolations)
 
@@ -16,5 +15,21 @@ describe('Profile', () => {
   it('has no a11y violations', async () => {
     const results = await axe(container)
     expect(results).toHaveNoViolations()
+  })
+
+  it('returns static props', async () => {
+    const result = await getStaticProps({ locale: 'en' })
+    expect(result.props).toBeTruthy()
+    expect(result.props.locale).toBe('en')
+    expect(result.props.langToggleLink).toBe('/fr/profile')
+    expect(result.props.metadata).toBeTruthy()
+    expect(result.props.breadCrumbItems).toHaveLength(1)
+  })
+
+  it('returns static french props', async () => {
+    const result = await getStaticProps({ locale: 'fr' })
+    expect(result.props).toBeTruthy()
+    expect(result.props.locale).toBe('fr')
+    expect(result.props.langToggleLink).toBe('/profile')
   })
 })
