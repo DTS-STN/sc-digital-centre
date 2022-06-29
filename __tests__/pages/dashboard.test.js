85a2aba2d23b8756f116e0b76a89fa581ccb33fd
Curtis Underwood <curtisund@gmail.com>
2022-06-29 12:44:47 -0400

    added test

--- __tests__/pages/dashboard.test.js
+++ __tests__/pages/dashboard.test.js
@@ -4,8 +4,14 @@ import { axe, toHaveNoViolations } from 'jest-axe'
 import { getAdvertsingCards } from '../../contents/BenefitAdvertisingCards'
 import Dashboard from '../../pages/dashboard'
 import { getNoBenefitCards } from '../../contents/NoBenefitCards'
+import NoBenefitCard from '../../components/molecules/NoBenefitCard'
+import { getCookie } from 'cookies-next'
 
 expect.extend(toHaveNoViolations)
+
+jest.mock('cookies-next', () => ({
+  getCookie: () => 'default',
+}))
 
 describe('Dashboard', () => {
   const { container } = render(
@@ -19,8 +25,26 @@ describe('Dashboard', () => {
       }}
     />
   )
+
+  const NoBenefitCards =
+    getCookie() == 'default'
+      ? render(
+          getNoBenefitCards('en').map((value, index) => {
+            return (
+              <div key={index}>
+                <NoBenefitCard locale="en" benefit={value} />
+              </div>
+            )
+          })
+        )
+      : null
+
   it('renders Dashboard', () => {
     expect(container).toBeTruthy()
+  })
+
+  it('renders NoBenefitCards', () => {
+    expect(NoBenefitCards).toBeTruthy()
   })
 
   it('has no a11y violations', async () => {
