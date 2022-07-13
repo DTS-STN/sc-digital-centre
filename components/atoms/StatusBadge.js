1b42952d3f02b2d94d46f5668785deb49507069f
shewood <shewood01@gmail.com>
2022-07-13 15:31:27 -0400

    status badge small change

--- components/atoms/StatusBadge.js
+++ components/atoms/StatusBadge.js
@@ -9,7 +9,7 @@ export default function StatusBadge(props) {
   return (
     <div className="flex justify-end rounded-t-lg">
       <h2
-        className={`text-black text-xl px-5 py-1.5 w-full sm:w-1/2 sm:mr-14 rounded-t-lg sm:rounded-t-none sm:rounded-b-2xl 
+        className={`text-black text-xl font-medium px-5 py-1.5 w-full sm:w-1/2 sm:mr-14 rounded-t-lg sm:rounded-t-none sm:rounded-b-2xl 
         ${props.color} `}
       >
         <span className="sr-only">{props.srDescription} </span>
