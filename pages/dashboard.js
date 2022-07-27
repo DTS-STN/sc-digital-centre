5f3736b5a3d652af8d81c220e1b9907c8aa8a024
tishamIslam <tisham.islam@gmail.com>
2022-07-27 13:01:33 -0400

    Removed console log

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -156,7 +156,6 @@ export default function Dashboard(props) {
                 orderedActiveAgreement.push(benefit)
                 break
             }
-            console.log(benefit)
             determineAdCards(benefit, defaultDisplayFlags)
           })
         }
