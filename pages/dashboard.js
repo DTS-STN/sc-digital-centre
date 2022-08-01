5cb843bc497148575124d8ea2425785add77adbe
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2022-08-01 11:07:02 -0400

    AEM intergration.

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -19,6 +19,7 @@ import { determineAdCards } from '../lib/mapAdCards'
 import MapCallout from '../lib/mapCallout'
 import { AuthIsDisabled, AuthIsValid, Redirect } from '../lib/auth'
 import LoadingState from '../components/molecules/LoadingState'
+import MapDashboard from '../lib/aem/mapper'
 // import queryGraphQL from '../graphql/client'
 // import getDashboardPage from '../graphql/queries/dashboardQuery.graphql'
 
@@ -290,10 +291,8 @@ export async function getServerSideProps({ req, res, locale, query }) {
   const { userid } = query
   setCookie('userid', userid, { req, res, maxAge: 60 * 6 * 24 })
 
-  // const aemContent = await queryGraphQL(getDashboardPage).then((result) => {
-  //   return result;
-  // });
-  // console.log(aemContent)
+  // Get mapped content from AEM
+  const aemContent = await MapDashboard()
 
   const metadata = {
     title: 'Digital Centre (en) + Digital Centre (fr)',
@@ -308,6 +307,7 @@ export async function getServerSideProps({ req, res, locale, query }) {
       isAuth: true,
       locale,
       metadata,
+      aemContent,
     },
   }
 }
