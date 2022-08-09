b5749caf9408fd9c68998804afa06870e601fd99
tishamIslam <tisham.islam@gmail.com>
2022-08-09 13:41:44 -0400

    Resolved typo in next config

--- next.config.js
+++ next.config.js
@@ -53,7 +53,7 @@ const config = {
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
-    localDetection: true,
+    localeDetection: true,
   },
   //disable X-Powered-By
   poweredByHeader: false,
