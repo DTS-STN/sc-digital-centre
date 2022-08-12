1ee64c9bf5d1660ff4dcfca920b10f1e2e690a75
Curtis Underwood <curtisund@gmail.com>
2022-08-12 15:44:36 -0400

    added strict csp scripts

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { generateNonce, generateCSP } from '../lib/Utils.js'
 import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    //set csp
+    const nonce = generateNonce()
+    ctx.res.setHeader('Content-Security-Policy', generateCSP({ nonce }))
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const additionalProps = { nonce }
+
+    return { ...initialProps, ...additionalProps }
   }
 
   render() {
+    const { nonce } = this.props
+
     return (
       <Html className="no-js" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
         <Head prefix="og:http://ogp.me/ns#"></Head>
@@ -17,11 +25,19 @@ class MyDocument extends Document {
           <script
             async
             src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"
+            nonce={nonce}
+            dangerouslySetInnerHTML={{
+              __html: `window.__webpack_nonce__ = "${nonce}"`,
+            }}
           ></script>
           {process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL ? (
             <Script
               strategy="beforeInteractive"
               src={process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL}
+              nonce={nonce}
+              dangerouslySetInnerHTML={{
+                __html: `window.__webpack_nonce__ = "${nonce}"`,
+              }}
             />
           ) : null}
         </body>
