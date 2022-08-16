36698382b75bafb278d47170ea5a0d5f26fadfd1
Curtis Underwood <curtisund@gmail.com>
2022-08-16 10:57:41 -0400

    fixed tests

--- pages/_document.js
+++ pages/_document.js
@@ -4,34 +4,37 @@ import Script from 'next/script'
 
 let prod = process.env.NODE_ENV == 'production'
 
+function getCsp(nonce) {
+  let csp = ``
+  csp += `base-uri 'self';`
+  csp += `form-action 'self';`
+  csp += `default-src 'self';`
+  csp += `script-src 'self' 'nonce-${nonce}' ${prod ? '' : "'unsafe-eval'"};`
+  csp += `style-src 'self' fonts.googleapis.com ${
+    prod ? '' : "'unsafe-inline'"
+  };`
+  csp += `img-src 'self' data: blob:;`
+  csp += `font-src 'self' https://fonts.gstatic.com data:;`
+  return csp
+}
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    //set csp
-    const nonce = generateNonce()
-
-    let csp = ``
-    csp += `base-uri 'self';`
-    csp += `form-action 'self';`
-    csp += `default-src 'self' ${prod ? '' : "'unsafe-eval'"};`
-    csp += `script-src-elem 'self' 'nonce-${nonce}';`
-    csp += `style-src 'self' 'nonce-${nonce}';`
-    csp += `img-src 'self' data: blob:;`
-    csp += `font-src 'self' https://fonts.gstatic.com data:;`
-    csp += `frame-ancestors 'self';`
-
-    ctx.res.setHeader('Content-Security-Policy', csp)
     const initialProps = await Document.getInitialProps(ctx)
-    const additionalProps = { nonce }
 
-    return { ...initialProps, ...additionalProps }
+    return { ...initialProps }
   }
 
   render() {
-    const { nonce } = this.props
+    const nonce = generateNonce()
+    let referrer = 'strict-origin'
 
     return (
       <Html className="no-js" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
-        <Head nonce={nonce} prefix="og:http://ogp.me/ns#"></Head>
+        <Head nonce={nonce} prefix="og:http://ogp.me/ns#">
+          <meta httpEquiv="Content-Security-Policy" content={getCsp(nonce)} />
+          <meta name="referrer" content={referrer} />
+        </Head>
         <body>
           <Main />
           <NextScript />
